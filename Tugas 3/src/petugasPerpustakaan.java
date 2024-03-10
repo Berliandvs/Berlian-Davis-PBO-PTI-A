6156c434bd9a9a369ccaec53c64c44c3670b7b56
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-03-10 21:22:53 +0700

    Create petugasPerpustakaan.java

---
+++ Tugas 3/src/petugasPerpustakaan.java
@@ -0,0 +1,23 @@
+public class petugasPerpustakaan {
+    private String namaPetugas;
+    private String jabatanPetugas;
+    private double gajiPetugas;
+    private String jamOperasional;
+
+    public void karirPetugas() {
+        // Implementasi untuk menampilkan karir petugas
+        namaPetugas = ("Davis");
+        jabatanPetugas = ("Kepala Perpustakaan");
+        gajiPetugas = (1000000.00);
+        jamOperasional = ("08.00-09.00 WIB");
+        System.out.println("Nama Petugas: " + namaPetugas);
+        System.out.println("Jabatan: " + jabatanPetugas);
+        System.out.println("Gaji: Rp" + gajiPetugas);
+        System.out.println("Jam Operasional: " + jamOperasional);
+    }
+
+    public void menambahkanBuku(int jumlahBuku) {
+        // Implementasi untuk menambahkan buku dengan memanggil variabel jumlahBuku
+        System.out.println("Menambahkan " + jumlahBuku + " buku ke dalam perpustakaan.");
+    }
+}
