2f95b5140774c8976653affc4d6f5304bcc4d386
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-03-25 23:56:57 +0700

    Create LIngkaranMain.java

---
+++ Tugas 4/TugasModule3/LIngkaranMain.java
@@ -0,0 +1,14 @@
+public class LIngkaranMain {
+    public static void main(String[] args) {
+        // NO 3
+        lingkaran IString = new lingkaran("3", "10");
+        IString.displayMessage();
+
+        lingkaran l = new lingkaran(3);
+        l.setTinggi(10);
+        l.displayMessage();
+
+        lingkaran l2 = new lingkaran(4, 10);
+        l2.displayMessage();
+    }
+}
