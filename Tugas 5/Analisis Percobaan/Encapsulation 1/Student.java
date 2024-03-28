d5f1b43ae832c7c9a41ebf10bed6820d3fd227a0
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-03-28 15:52:31 +0700

    Update Student.java

--- Tugas 5/Analisis Percobaan/Encapsulation 1/Student.java
+++ Tugas 5/Analisis Percobaan/Encapsulation 1/Student.java
@@ -1,1 +1,20 @@
+// NO 4 (bisa)
+package Encapsulation1;
 
+public class Student { 
+    private String name; 
+    private int mark;
+    // NO 3 (jadi tidak bisa diakses di kelas lain)
+    public void setName(String n){ 
+       name=n; 
+    } 
+    public String getName(){ 
+       return name; 
+    }     
+    public void setMark(int m){ 
+       mark=m; 
+    }     
+    public int getMark(){ 
+       return mark; 
+    } 
+}
