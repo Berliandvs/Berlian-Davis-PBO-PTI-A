d7e5c8db27ac5cb24831c152f9734ab3ded58703
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-05-08 19:37:15 +0700

    Create KuePesanan.java

---
+++ Tugas 7/Praktikum/KuePesanan.java
@@ -0,0 +1,20 @@
+class KuePesanan extends Kue {
+    private double berat;
+
+    public KuePesanan(String nama, double harga, double berat) {
+        super(nama, harga);
+        this.berat = berat;
+    }
+
+    public double hitungHarga() {
+        return getHarga() * berat;
+    }
+
+    public double getBerat() {
+        return berat;
+    }
+
+    public void setBerat(double berat) {
+        this.berat = berat;
+    }
+}
