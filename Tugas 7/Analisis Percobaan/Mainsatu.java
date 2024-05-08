3272f59fc4c2caf161fb3323082e5309f91478f6
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-05-08 19:32:08 +0700

    Create Mainsatu.java

---
+++ Tugas 7/Analisis Percobaan/Mainsatu.java
@@ -0,0 +1,9 @@
+// public class Mainsatu {
+//     public static void main(String[] args) {
+//         Employee employee = new Employee();
+//     }
+// }
+
+// terjadi kesalahan, Ketika dijalankan eror,
+// karena pada class main yang dipanggil adalah kelas employee yang dimana class employee adalah abstract class.
+// Abstract class tidak dapat diinstansiasi, yang artinya tidak dapat membuat objek langsung dari class abstract
