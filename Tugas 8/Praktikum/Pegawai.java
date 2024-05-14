4896fd98193cc5b55bb538adc2a2d90e1db07fb9
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-05-14 08:14:08 +0700

    Create Pegawai.java

---
+++ Tugas 8/Praktikum/Pegawai.java
@@ -0,0 +1,25 @@
+// Abstract class Pegawai
+abstract class Pegawai {
+    private String nama;
+    private String noKTP;
+
+    public Pegawai(String nama, String noKTP) {
+        this.nama = nama;
+        this.noKTP = noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public abstract double gaji(); // Abstract method
+
+    @Override
+    public String toString() {
+        return "No KTP: " + noKTP;
+    }
+}
