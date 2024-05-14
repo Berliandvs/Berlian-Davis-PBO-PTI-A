32018b6ebf005122e916b712420dcc46b223047e
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-05-14 08:14:34 +0700

    Create PegawaiHarian.java

---
+++ Tugas 8/Praktikum/PegawaiHarian.java
@@ -0,0 +1,37 @@
+// Subclass PegawaiHarian
+class PegawaiHarian extends Pegawai {
+    private double upahPerJam;
+    private int totalJam;
+
+    public PegawaiHarian(String nama, String noKTP, double upahPerJam, int totalJam) {
+        super(nama, noKTP);
+        this.upahPerJam = upahPerJam;
+        this.totalJam = totalJam;
+    }
+
+    public void setUpahPerJam(double upahPerJam) {
+        this.upahPerJam = upahPerJam;
+    }
+
+    public double getUpahPerJam() {
+        return upahPerJam;
+    }
+
+    public void setTotalJam(int totalJam) {
+        this.totalJam = totalJam;
+    }
+
+    public int getTotalJam() {
+        return totalJam;
+    }
+
+    @Override
+    public double gaji() {
+        return upahPerJam * totalJam;
+    }
+
+    @Override
+    public String toString() {
+        return "Pegawai Harian: " + getNama() + "\n" + super.toString() + "\nUpah/jam: Rp." + upahPerJam + "\nTotal jam kerja: " + totalJam + "\nPendapatan: Rp." + gaji();
+    }
+}
