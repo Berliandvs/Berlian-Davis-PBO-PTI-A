72c14914fa167f8e61fb7eab0e90a12c5e8ffba4
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-05-19 23:39:35 +0700

    Create PermanentEmployee.java

---
+++ Tugas 9/Praktikum/PermanentEmployee.java
@@ -0,0 +1,30 @@
+// Class PermanentEmployee
+public class PermanentEmployee extends Employee {
+    private String retirementPlan;
+
+    public PermanentEmployee(int registrationNumber, String name, int salaryPerMonth, Invoice[] invoices, String retirementPlan) {
+        super(registrationNumber, name, salaryPerMonth, invoices);
+        this.retirementPlan = retirementPlan;
+    }
+
+    public String getRetirementPlan() {
+        return retirementPlan;
+    }
+
+    public void setRetirementPlan(String retirementPlan) {
+        this.retirementPlan = retirementPlan;
+    }
+
+    public void uniqueMethod1() {
+        System.out.println("PermanentEmployee unique method 1.");
+    }
+
+    public void uniqueMethod2() {
+        System.out.println("PermanentEmployee unique method 2.");
+    }
+
+    @Override
+    public void abstractMethod() {
+        System.out.println("PermanentEmployee implementation of abstract method.");
+    }
+}
