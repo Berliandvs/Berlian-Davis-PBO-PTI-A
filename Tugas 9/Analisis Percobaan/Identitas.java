370923cf6c59af4055cb6738418bba0808b4205d
Berliandvs <160575797+Berliandvs@users.noreply.github.com>
2024-05-19 23:35:13 +0700

    Create Identitas.java

---
+++ Tugas 9/Analisis Percobaan/Identitas.java
@@ -0,0 +1,8 @@
+public interface Identitas {
+    public void tampilkanNama();
+    //NO 4, maka akan terjadi kesalahan di class Manusia, karena di class Manusia
+    //memanggil method tampilkanNama
+    public void tampilkanUmur();
+    public void tampilkanJenisKelamin();
+   }
+// NO 1 tidak terjadi kesalahan, tetapi dalam kode program yang diberikan karena main program tidak ada maka tidak bisa dijalankan
